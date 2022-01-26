e6ab83beddccec18b7511f83699d8ab18801ae73
Sithembiso <ssithembisosifiso@gmail.com>
2022-01-26 10:39:32 +0200

    names added

--- home.js
+++ home.js
@@ -1,78 +1,8 @@
-let name = [];
-let year =[];
-let select = [];
+var people = ["Happy 23 year","Nkosiphandule 65 year","Sithembiso 43 year","Andiswe 32 year","Sophie 30 year","Mduduzi 98 year","Wandile 26 year","Vusi 75 year","Precious 34 year","Selina 23 year","Kagisho 75 year","Mhlompheni 27 year","Lucky 89 year","Lebo 56 year","Khumbulani 90 year",];
+var name = [];
+var display = document.getElementById("display");
 
-function getTeamValues(){
-    name = document.getElementById("name").value;
-    year = document.getElementById("year").value;
-    select = document.getElementById("select");
+for (let i= 0; i < people.length; i++ ) {
+    display.innerHTML += "<li>" + people[i] + "</li>";
 }
 
-function displayTable() {
-    const table = document.getElementById("select");
-    table.innerHTML = "";
-    for (let i = 0; i < select.length; i++) {
-        table.innerHTML +=
-            "<tr><td>" +
-            select[i].name +
-            "</td><td>" +
-            select[i].year +
-            "</td></tr>";
-    }
-}
-
-function home() {
-    getTeamValues()
-
-    if (name == "" || year == "") {
-        alert("Name can't be clear");
-        return false;
-    }
-    orders.push({ name: "home", name: name, year: year});
-    displayTable();
-}
-
-
-
-let name = [];
-let year = [];
-
-function year() {
-    
-    const name = document.getElementById('name').value;
-    const year = document.getElementById('year').value;
-
-  
-  let people = [
-    { name: 'hardy', year: 55 },
-    { name: 'vusumuzi', year: 2 },
-    { name: 'wandile', year: 65 },
-    { name: 'charity', year: 28 },
-    { name: 'precious', year: 29 },
-    { name: 'andisiwe', year:  30 },
-    { name: 'mmapaseka', year: 31 },
-    { name: 'mhlonipheni', year: 35 },
-    { name: 'khumbulani', year: 25 },
-    { name: 'sophie', year: 37 },
-    { name: 'lucky', year: 32 },
-    { name: 'sithembiso', year: 32 },
-    { name: 'happy', year: 18 },
-    { name: 'selina', year: 26 },
-    { name: 'nkosiphendule', year: 30 },
-    { name: 'thato', year: 30 },
-    { name: "Willism", year: 26 },
-    { name: "Sibusiso", year: 27 },
-  ];
-  
-  for (let i = 0; i < people.length; i++) {
-      
-    if ((people[i].name == name) && (people[i].year == year)) {
-        people.innerHTML += '<li>' + people[i].name + ' year ' + people[i].year + '</li>';
-        location.href = 'home.html';
-    }
-    function name() {
-        location.href = "index.html";
-      }
-
-  }
-}
--- login.js
+++ login.js
@@ -1,43 +1,13 @@
-// this will run as soon your app shows in the browser
-// you can use it to call or execute some functions
-document.addEventListener('DOMContentLoaded', () => {
-  console.log(`Hello Team...`)
-// this code stops your <form> from reloading the page
-  document.querySelector('#form').addEventListener('submit', (event) => {
-    event.preventDefault();
-  });
+var names = [];
 
-});
 function login() {
-    
-    const name = document.getElementById('name').value;
-    const select = document.getElementById('select').value;
 
-  
-  let people = [
-    { name: 'hardy', age: 10, color: 'blue', hairStyle: 'dread lock' },
-    { name: 'vusumuzi', age: 21, color: 'red', hairStyle: 'chiskop' },
-    { name: 'wandile', age: 65, color: 'blue', hairStyle: 'chiskop' },
-    { name: 'charity', age: 28, color: 'pink', hairStyle: 'afro' },
-    { name: 'precious', age: 17, color: 'pink', hairStyle: 'braid' },
-    { name: 'andisiwe', age: 90, color: 'green', hairStyle: 'afro' },
-    { name: 'mmapaseka', age: 30, color: 'black', hairStyle: 'braid' },
-    { name: 'mhlonipheni', age: 20, color: 'yellow', hairStyle: 'afro' },
-    { name: 'khumbulani', age: 30, color: 'blue', hairStyle: 'afro' },
-    { name: 'sophie', age: 18, color: 'grey', hairStyle: 'braid' },
-    { name: 'lucky', age: 32, color: 'grey', hairStyle: 'chiskop' },
-    { name: 'sithembiso', age: 32, color: 'blue', hairStyle: 'chiskop' },
-    { name: 'happy', age: 18, color: 'white', hairStyle: 'twist' },
-    { name: 'selina', age: 26, color: 'green', hairStyle: 'chiskop' },
-    { name: 'nkosiphendule', age: 30, color: 'white', hairStyle: 'chiskop' },
-    { name: 'thato', age: 30, color: 'green', hairStyle: 'chiskop' },
-  ];
-  
-  for (let i = 0; i < people.length; i++) {
-      
-    if ((people[i].name == name) && (people[i].select == select)) {
-        people.innerHTML += '<li>' + people[i].name + ' select ' + people[i].select + '</li>';
-        location.href = 'home.html';
-    }
+  if (names == "") {
+    alert("Email or Password is incorrect");
+    return false;
+    
+  } else {
+      return true;
   }
+  
 }
