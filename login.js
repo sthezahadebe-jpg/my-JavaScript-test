b194d2458c87afa779234b3a20aa693ce124a9dc
Sithembiso <ssithembisosifiso@gmail.com>
2022-01-27 14:46:46 +0200

    password added

--- login.js
+++ login.js
@@ -1,16 +1,27 @@
+var email = [];
+var password = [];
+
 
 function login() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
-  
-  if (email === "" || password === "") {
+
+  if (email === "" && password === "") {
     alert("Email or Password is incorrect");
   } else {
 
-    location.href = "home.html";
   }
+
+  if (email == "user@gmail.com" && password == "mypassword") {
+    window.location.href = "home.html"
+  }
+  else {
+    alert("email & password are incorrect");
+  }
+
 }
+
 
 
 
